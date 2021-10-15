c1b4316c7eed54d0a40a98a4d04eea7e0025fe3b
Firewall Marsha <fmarsha@oa-l1.msk.21-school.ru>
2021-10-15 23:36:29 +0300

    2

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,10 +14,7 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-
-	i = -1;
-	while (++i < n)
-		((char *)dest)[i] = ((char *)src)[i];
+	while (n--)
+		((char *)dest)[n] = ((char *)src)[n];
 	return (dest);
 }
