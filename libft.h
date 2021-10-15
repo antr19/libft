536c039bfcb030a6aea4e78f06d1b431dd0f4440
Firewall Marsha <fmarsha@am-j4.msk.21-school.ru>
2021-10-16 00:45:50 +0300

    3

--- libft.h
+++ libft.h
@@ -24,5 +24,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
